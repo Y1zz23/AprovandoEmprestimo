cf0c1bed85b04b34243f6b9009e2c78a38491a56
Matheus Ricardo <matheusri1@hotmail.com>
2023-05-18 18:07:59 -0300

    Update AprovandoEmpréstimo.py

--- AprovandoEmpréstimo.py
+++ AprovandoEmpréstimo.py
@@ -1,9 +1,9 @@
-casa = int(input("Qual o valor da casa: "))
-salario = int(input("Qual o seu salário: "))
+casa = int(input("Qual o valor da casa: R$"))
+salario = int(input("Qual o seu salário: R$ "))
 prestacao = int(input("Em quantos meses voce ira pagar: "))
 mensal = casa/prestacao
 minimo = salario * 0.3
-print("Voce terá que pagar {:.2f} em {} meses".format(minimo,prestacao))
+print("Voce terá que pagar {:.2f} em {} meses".format(mensal,prestacao))
 if mensal >= minimo:
     print("Voce nao pode financiar esta casa")
 else:
